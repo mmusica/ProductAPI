5ee9e12b723e7d8b67e8f1e52eb39eebd464f23c
Marko Mušica <markomusica@gmail.com>
2023-03-11 19:07:29 +0100

    Added ef migration and updated HTTP requests to work with local database

--- ProductAPI/Controllers/ProductController.cs
+++ ProductAPI/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Azure.Core;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ProductAPI.Controllers
 {
@@ -6,37 +7,62 @@ namespace ProductAPI.Controllers
     [ApiController]
     public class ProductController : ControllerBase
     {
+        private readonly DataContext _context;
+        public ProductController(DataContext context)
+        {
+            _context = context;
+        }
+
         private static List<Product> products = new List<Product>
         {
                 new Product { Id = 1, Name="Milk", Quantity= 1, Price=2},
                 new Product { Id = 2, Name="Bread", Quantity= 2, Price=1}
             };
+        
+
         [HttpGet]
         public async Task<ActionResult<List<Product>>> Get()
         {
 
-            return Ok(products);
+            //return Ok(products); no database
+            return Ok(await _context.Products.ToListAsync());
         }
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
+            /*
             var product = products.Find(x => x.Id == id);
             if (product == null)
             {
                 return BadRequest("Product not found");
             }
-            return Ok(product);
+            return Ok(product);*/ //no database
+            
+            var product = await _context.Products.FindAsync(id);
+            //var product = await (from c in _context.Products where c.Id == id select c).ToListAsync(); using linq
+            if (product == null)
+            {
+                return BadRequest("Product not found");
+            }
+            
+            return Ok(product); 
         }
 
         [HttpPost]
         public async Task<ActionResult<List<Product>>> AddProduct(Product product)
         {
+            /*
             products.Add(product);
-            return Ok(products);
+            return Ok(products);*/ //No Database
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+            return Ok(await _context.Products.ToListAsync());
+
         }
         [HttpPut]
         public async Task<ActionResult<List<Product>>> UpdateProduct(Product request)
         {
+            /*
             var foundProduct = products.Find(x => x.Id == request.Id);
             if (foundProduct == null)
             {
@@ -49,11 +75,26 @@ namespace ProductAPI.Controllers
                 foundProduct.Quantity = request.Quantity;
                 foundProduct.Price = request.Price;
             }
-            return Ok(products);
+            return Ok(products);*/ //no database
+
+            var foundProductDb = await _context.Products.FindAsync(request.Id);
+            if (foundProductDb == null)
+            {
+                return BadRequest("Product not found");
+            }
+            else
+            {
+                foundProductDb.Name = request.Name;
+                foundProductDb.Price = request.Price;
+                foundProductDb.Quantity = request.Quantity;
+                await _context.SaveChangesAsync();
+            }
+            return Ok(await _context.Products.ToListAsync());
         }
         [HttpDelete("{id}")]
         public async Task<ActionResult<List<Product>>> DeleteProduct(int id)
         {
+            /*
             var foundProduct = products.Find(x => x.Id == id);
             if (foundProduct == null)
             {
@@ -64,7 +105,19 @@ namespace ProductAPI.Controllers
                 products.Remove(foundProduct);
             }
            
-            return Ok(products);
+            return Ok(products);*/ //no database
+
+            var foundProductDb = await _context.Products.FindAsync(id);
+            if (foundProductDb == null)
+            {
+                return BadRequest("Product not found");
+            }
+            else
+            {
+                _context.Remove(foundProductDb);
+                await _context.SaveChangesAsync();
+            }
+            return Ok(await _context.Products.ToListAsync());
         }
 
     }
--- ProductAPI/Data/DataContext.cs
+++ ProductAPI/Data/DataContext.cs
@@ -4,5 +4,18 @@ namespace ProductAPI.Data
 {
     public class DataContext : DbContext
     {
+        public DataContext()
+        {
+
+        }
+        public DataContext(DbContextOptions<DbContext> options) : base(options) {
+        }
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+
+
     }
 }
